e287cf00e9405498c5e77f615f3842067b039479
hunseok329 <kha5064@gmail.com>
2020-04-17 20:02:12 +0900

    make auto slideShow part

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var showIndex = 1;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+
 showSlides(showIndex);
+setTimeout(showAutoSlide, 4000);
 
 //슬라이드 방향 값을 가져와서 실행
 function plusSlides(n) {
@@ -13,8 +17,6 @@ function currentSlide(n) {
 //일정한 값을 받을면 그에 대한 동작을 해주는 함수
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     showIndex = 1;
   }
@@ -29,4 +31,21 @@ function showSlides(n) {
   }
   slides[showIndex - 1].style.display = "block";
   dots[showIndex - 1].className += " active";
+}
+//자동으로 슬라이드쇼 진행
+function showAutoSlide() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  showIndex++;
+  if (showIndex > slides.length) {
+    showIndex = 1;
+  }
+  slides[showIndex - 1].style.display = "block";
+  dots[showIndex - 1].className += " active";
+  setTimeout(showAutoSlide, 4000);
 }
